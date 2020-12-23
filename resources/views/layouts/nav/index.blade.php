cc6f41975f65f7a7b5da455690b43b82f43f3737
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-24 01:22:05 +0600

    Create nav components

---
+++ resources/views/layouts/nav/index.blade.php
@@ -0,0 +1,12 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+    <div class="sidebar-sticky pt-3">
+        <ul class="nav flex-column">
+            @include('layouts.nav.items.dashboard')
+            @include('layouts.nav.items.access-log')
+            
+            @foreach ((new App\Helpers\ViewHelpers\NavBuilder)->getNavItems() as $item)
+                @include('layouts.nav.items.' . $item)
+            @endforeach
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/layouts/nav/items/user.blade.php
@@ -0,0 +1,5 @@
+<a class="nav-item container-fluid" href="{{ route('users') }}">
+    <li class="nav-link text-white {{ request()->url() == route('users') ? 'active' : '' }} row">
+        <i class="fas fa-user pr-1 col-2 my-auto"></i><span class="d-inline-block col-10">Users {{ request()->url() == route('users') ? '<span class="sr-only">(current)</span>' : '' }}</span>
+    </li>
+</a>
