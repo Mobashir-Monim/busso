c1934451b53ecdad3db87e448b2c8fed35c3264f
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-24 02:35:35 +0600

    Update home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function needToImplement()
+    {
+        return view('need-to-implement');
+    }
 }
