77689e27414213b2970b634e8c741cbe2c6fe4d3
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-24 02:21:35 +0600

    Upate home view and a tbd view

---
+++ resources/views/need-to-implement.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header bg-dark text-white">TBD</div>
+
+                    <div class="card-body text-center">
+                        This feature has not been implemented yet. <br>
+                        Please wait for the developers to complete it.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
