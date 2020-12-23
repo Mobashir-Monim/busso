fae863bba79b8ff9f1d69bce83a9f7ca3d1c68d5
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-24 02:35:10 +0600

    Add nav helper

---
+++ app/Helpers/ViewHelpers/NavBuilder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Helpers\ViewHelpers;
+
+use App\Helpers\Helper;
+
+class NavBuilder extends Helper
+{
+    protected $navItems = [
+        'super-admin' => [
+            'resource-group',
+            'role',
+            'scope',
+            'user-attribute',
+            'user-attribute-value',
+            'user',
+            'claim'
+        ],
+        'user-admin' => [
+            'user',
+            'role',
+            'user-attribute',
+            'user-attribute-value',
+        ],
+        'resource-admin' => [
+            'resource-group',
+            'scope',
+            'claim',
+            'user-attribute-value',
+        ]
+    ];
+
+    public function getNavItems()
+    {
+        return $this->navItems[auth()->user()->roles->where('is_system_role', true)->first()->name];
+    }
+}
