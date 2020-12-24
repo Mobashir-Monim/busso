63666c587c2fafef26031713a8737173d827fe91
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-24 22:19:37 +0600

    Update nav layouts

--- resources/views/layouts/nav/items/claim.blade.php
+++ resources/views/layouts/nav/items/claim.blade.php
@@ -1,5 +1,5 @@
 <a class="nav-item container-fluid" href="{{ route('claims') }}">
-    <li class="nav-link text-white {{ request()->url() == route('claims') ? 'active' : '' }} row">
+    <li class="nav-link text-white {{ startsWith(request()->url(), route('claims')) ? 'active' : '' }} row">
         <i class="fa fas fa-project-diagram pr-1 col-2 my-auto"></i><span class="d-inline-block col-10">SAML Claims {!! request()->url() == route('claims') ? '<span class="sr-only">(current)</span>' : '' !!}</span>
     </li>
 </a>
--- resources/views/layouts/nav/items/role.blade.php
+++ resources/views/layouts/nav/items/role.blade.php
@@ -1,5 +1,5 @@
 <a class="nav-item container-fluid" href="{{ route('roles') }}">
-    <li class="nav-link text-white {{ request()->url() == route('roles') ? 'active' : '' }} row">
+    <li class="nav-link text-white {{ startsWith(request()->url(), route('roles')) ? 'active' : '' }} row">
         <i class="fas fa-exclamation-triangle pr-1 col-2 my-auto"></i><span class="d-inline-block col-10">Role {!! request()->url() == route('roles') ? '<span class="sr-only">(current)</span>' : '' !!}</span>
     </li>
 </a>
