9b520ad5361388b030efda1979cc9928da357796
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-24 23:49:08 +0600

    Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="border-bottom border-3 border-primary">Dashboard</h3>
+        </div>
+    </div>
     <div class="row mb-4">
-        <div class="col-md-8">
+        <div class="col-md-8 my-2">
             <div class="card card-rounded">
                 <div class="card-body">
-                    <h3 class="border-bottom border-3 border-primary">{{ auth()->user()->name }}</h3>
+                    <h4 class="border-bottom border-3 border-primary">{{ auth()->user()->name }}</h4>
                     <div class="row">
                         <div class="col-md-4 mb-2 d-md-none"><b class="border-bottom d-block">Email Address:</b></div>
                         <div class="col-md-4 mb-2 d-none d-md-block"><b>Email Address:</b></div>
@@ -27,6 +32,14 @@
                             @endif
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md"></div>
+        <div class="col-md-2 my-2">
+            <div class="card card-rounded">
+                <div class="card-body p-0">
+                    <img src="/img/user-image.jpg" class="img-fluid card-rounded" alt="{{ auth()->user()->name }} Image">
                 </div>
             </div>
         </div>
