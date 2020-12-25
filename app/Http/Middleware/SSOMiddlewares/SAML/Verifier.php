bf686d34fccf516ded6f9d44a44001886d24a22f
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-25 21:39:20 +0600

    Add debug code

--- app/Http/Middleware/SSOMiddlewares/SAML/Verifier.php
+++ app/Http/Middleware/SSOMiddlewares/SAML/Verifier.php
@@ -62,6 +62,7 @@ class Verifier
 
     public function generateCheckArray()
     {
+        dd($this->entity);
         return [
             [is_null($this->entity), 404],
             [request()->url() != $this->destination, 400],
