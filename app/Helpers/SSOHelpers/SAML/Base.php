3cf26c410804e2ed2229e50b0e84eacada67e5e1
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-27 04:21:04 +0600

    Debug

--- app/Helpers/SSOHelpers/SAML/Base.php
+++ app/Helpers/SSOHelpers/SAML/Base.php
@@ -64,7 +64,7 @@ class Base extends Helper
         $messageContext = new MessageContext();
         $messageContext->setMessage($response)->asResponse();
 
-        if (!is_null($type)) {
+        if (is_null($type)) {
             $sc = new SC;
             $response->serialize($sc->getDocument(), $sc);
             dd($messageContext, $response, base64_encode(gzdeflate($sc->getDocument()->saveXML())));
