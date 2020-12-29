82545b6ae088f4c2a7537e4aac1f70b8f9fd2bba
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-29 23:37:12 +0600

    Debug

--- app/Helpers/SSOHelpers/SAML/Base.php
+++ app/Helpers/SSOHelpers/SAML/Base.php
@@ -63,7 +63,7 @@ class Base extends Helper
     {
         $sc = new SC;
         $response->serialize($sc->getDocument(), $sc);
-        dd($sc);
+        dd($sc->getDocument()->saveXML());
         Http::post($this->entity->acs, [
             'SAMLResponse' => base64_encode(gzdeflate($response->saveXML())),
             'RelayState' => $response
