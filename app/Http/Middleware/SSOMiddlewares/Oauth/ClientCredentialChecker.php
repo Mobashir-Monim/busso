5c1c7acbec8cdf77eb1cbec183ebcbb36f52a816
Mobashir Monim <mobashirmonim@gmail.com>
2021-01-21 01:14:09 +0600

    Debug

--- app/Http/Middleware/SSOMiddlewares/Oauth/ClientCredentialChecker.php
+++ app/Http/Middleware/SSOMiddlewares/Oauth/ClientCredentialChecker.php
@@ -20,7 +20,6 @@ class ClientCredentialChecker
         $client = Passport::client()->find($request->client_id);
 
         if ($client->secret != $request->client_secret || $client->redirect != $request->redirect_uri) {
-            dd($client->secret, $request->client_secret, $client->redirect, $request->redirect_uri);
             return response()->json([
                 'success' => false,
             ], 401);
