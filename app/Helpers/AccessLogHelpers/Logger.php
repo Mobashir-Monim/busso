9ad371f1b373cc5d5a6ddebb2dd6808562a6846a
Mobashir Monim <mobashirmonim@gmail.com>
2021-01-27 15:43:56 +0600

    debug

--- app/Helpers/AccessLogHelpers/Logger.php
+++ app/Helpers/AccessLogHelpers/Logger.php
@@ -9,6 +9,7 @@ class Logger extends Helper
 {
     public function __construct($user, $group, $resource, $type)
     {
+        dd('here');
         $log = new AccessLog;
         $log->user_id = $user;
         $log->resource_group_id = $group;
