5965a202a0604dd5cec8a5e41c517d5a7e27db6f
Mobashir Monim <mobashirmonim@gmail.com>
2021-02-01 17:38:43 +0600

    Update password resetting redirect

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +21,31 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? $this->resetLinkSent($request, $response)
+                    : $this->sendResetLinkFailedResponse($request, $response);
+    }
+
+    public function resetLinkSent($request, $response)
+    {
+        flash('We have sent you a reset link. Please check your email inbox. Please also check your spam folder if you cannot find it in your inbox.')->success();
+
+        return $this->sendResetLinkResponse($request, $response);
+    }
+
+    public function resetLinkFailed($request, $response)
+    {
+        flash('We could not send the reset link.')->error();
+        
+        return $this->sendResetLinkFailedResponse($request, $response);
+    }
 }
