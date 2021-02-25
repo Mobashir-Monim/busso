5a9bae89f1d1ab479c47b9cf6e8d8a912134c815
Mobashir Monim <mobashirmonim@gmail.com>
2021-02-26 01:25:16 +0600

    Debug

--- resources/views/auth/sso/oauth.blade.php
+++ resources/views/auth/sso/oauth.blade.php
@@ -1,7 +1,1 @@
 <input type="hidden" name="stuff" value="{{ $oauth }}" id="stuff">
-
-<script>
-    window.onload = () => {
-        console.log('{!! json_encode((new App\Helpers\SSOHelpers\OAuth\Login)->authenticatorParamDecompressor($oauth)) !!}');
-    }
-</script>
