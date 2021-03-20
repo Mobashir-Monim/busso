c152c22b7df9dd96651732c6bed5c28080b55b6a
Mobashir Monim <mobashirmonim@gmail.com>
2021-03-21 00:50:03 +0600

    debug

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test', function () {
+    $x = file_get_contents("../storage/oauth-private.key");
+    echo $x;
+    dd();
+    JWT::encode($this->generateIDToken($auth_code, $access_token), $x, 'RS256');
     dd(file_get_contents("../storage/oauth-private.key"));
     dd('nothing in test');
 })->name('tester');
