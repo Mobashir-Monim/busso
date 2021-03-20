ddeb0e23443f04648755a65db96ac114c49a3c0a
Mobashir Monim <mobashirmonim@gmail.com>
2021-03-21 01:56:38 +0600

    debug

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test', function () {
+    dd(\Firebase\JWT\JWT::decode(request()->x, file_get_contents('../storage/oauth-public.key'), array('RS256')));
+    dd(base64url_decode($x), base64url_decode($y));
     dd('nothing in test');
 })->name('tester');
 
