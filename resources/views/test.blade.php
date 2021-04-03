75b1f258c3e96c30522b2de102ec95791f01c677
Mobashir Monim <mobashirmonim@gmail.com>
2021-04-03 15:13:33 +0600

    Debug

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    henlo
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Route</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Response</th>
+                        <th scope="col">Error</th>
+                        <th scope="col">Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach (App\Models\OidcResponseLogger as $item)
+                        <tr>
+                            <td>{{ $item->route }}</td>
+                            <td>{{ $item->data }}</td>
+                            <td>{{ $item->eesponse }}</td>
+                            <td>{{ $item->error ? '1' : '0' }}</td>
+                            <td>{{ $item->created_at }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
