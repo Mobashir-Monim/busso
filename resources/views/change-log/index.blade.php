48a1ca7f04510e4b7bfbd19e1c9532cb5b1eeed3
Mobashir Monim <mobashirmonim@gmail.com>
2021-08-19 03:19:12 +0600

    Update change log stype

--- resources/views/change-log/index.blade.php
+++ resources/views/change-log/index.blade.php
@@ -5,20 +5,22 @@
         <div class="col-md-12">
             <h2 class="border-bottom border-primary">Change Log</h2>
             @foreach ($logs as $log)
-                <div class="card card-rounded my-3">
+                <div class="card card-rounded card-hoverable my-3" onclick="window.open('{{ route('change-log.view', ['group' => $log->group_id]) }}', '_blank')">
                     <div class="card-body">
-                        <h4 class="border-bottom mb-0">{{ $log->type }}::{{ $log->mode }} ({{ $log->groupCount }})</h4>
                         <div class="row">
-                            <div class="col-md-12">
+                            <div class="col-md-10 border-right">
+                                <h4 class="border-bottom mb-0">
+                                    {{ $log->type }}::{{ $log->mode }} ({{ $log->groupCount }})
+                                </h4>
                                 <p class="text-muted mb-0 d-inline-block">
                                     {{ $log->group_id }}
                                 </p>
-                                <p class="text-primary mb-0 d-inline-block float-right">
-                                    {{ $log->user->name }}
-                                </p>
+                            </div>
+                            <div class="col-md-2 my-auto text-center">
+                                <p class="text-primary mb-0">{{ $log->user->name }}</p>
+                                {{-- <a href="{{ route('change-log.view', ['group' => $log->group_id]) }}" target="_blank" class="btn btn-sm btn-dark"><i class="fas fa-eye"></i></a> --}}
                             </div>
                         </div>
-                        <a href="{{ route('change-log.view', ['group' => $log->group_id]) }}" target="_blank" class="btn btn-dark float-right"><i class="fas fa-eye"></i></a>
                     </div>
                 </div>
             @endforeach
