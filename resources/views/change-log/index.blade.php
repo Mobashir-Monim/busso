fcce05b1ac066df76e731dd5e12e301edb5eff09
Mobashir Monim <mobashirmonim@gmail.com>
2021-09-02 11:08:20 +0600

    Modify change log index view style

--- resources/views/change-log/index.blade.php
+++ resources/views/change-log/index.blade.php
@@ -4,26 +4,33 @@
     <div class="row">
         <div class="col-md-12">
             <h2 class="border-bottom border-primary">Change Log</h2>
-            @foreach ($logs as $log)
-                <div class="card card-rounded card-hoverable my-3" onclick="window.open('{{ route('change-log.view', ['group' => $log->group_id]) }}', '_blank')">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-10 border-right">
-                                <h4 class="border-bottom mb-0">
-                                    {{ $log->type }}::{{ $log->mode }} ({{ $log->groupCount }})
-                                </h4>
-                                <p class="text-muted mb-0 d-inline-block">
-                                    {{ $log->group_id }}
-                                </p>
-                            </div>
-                            <div class="col-md-2 my-auto text-center">
-                                <p class="text-primary mb-0">{{ $log->user->name }}</p>
-                                {{-- <a href="{{ route('change-log.view', ['group' => $log->group_id]) }}" target="_blank" class="btn btn-sm btn-dark"><i class="fas fa-eye"></i></a> --}}
+            <div class="row">
+                @foreach ($logs as $log)
+                    <div class="col-md-6">
+                        <div class="card card-rounded card-hoverable my-3" onclick="window.open('{{ route('change-log.view', ['group' => $log->group_id]) }}', '_blank')">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-12 my-2">
+                                        <h4 class="border-bottom mb-0 d-flex justify-content-between">
+                                            <span>{{ $log->type }}::{{ $log->mode }}</span>
+                                            <span class="text-muted" style="font-size: 0.7em">
+                                                [ <span class="text-primary">{{ $log->groupCount }}</span> ]
+                                            </span>
+                                        </h4>
+                                        <p class="text-muted mb-0 text-right">
+                                            {{ $log->group_id }}
+                                        </p>
+                                        <p class="text-primary mb-0 d-flex justify-content-end bg-light" onclick="window.open('{{ route('users.show', ['user' => $log->user_id]) }}')">
+                                            <span class="material-icons-outlined mr-2" style="font-size: 19px">account_circle</span>
+                                            {{ $log->user->name }}
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
